8f506c8c7ac5bae6ddca29a8c30006c57501ae65
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-12-10 19:09:16 +0500

    Auto-commit: Day_4_Printing_Department.java updated

--- Advent of Code 2025/src/Day_4_Printing_Department.java
+++ Advent of Code 2025/src/Day_4_Printing_Department.java
@@ -29,30 +29,47 @@ public class Day_4_Printing_Department {
             grid[r] = lines.get(r).toCharArray();
         }
 
-        int accessibleCount = 0;
         int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+        int removedTotal = 0;
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] != '@') continue;
+        while (true) {
+            List<int[]> toRemove = new ArrayList<>();
 
-                int neighbors = 0;
-                for (int k = 0; k < 8; k++) {
-                    int nr = r + dr[k];
-                    int nc = c + dc[k];
-                    if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
-                    if (grid[nr][nc] == '@') {
-                        neighbors++;
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+                    if (grid[r][c] != '@') continue;
+
+                    int neighbors = 0;
+                    for (int k = 0; k < 8; k++) {
+                        int nr = r + dr[k];
+                        int nc = c + dc[k];
+                        if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
+                        if (grid[nr][nc] == '@') {
+                            neighbors++;
+                        }
+                    }
+
+                    if (neighbors < 4) {
+                        toRemove.add(new int[]{r, c});
                     }
                 }
+            }
 
-                if (neighbors < 4) {
-                    accessibleCount++;
+            if (toRemove.isEmpty()) {
+                break; // no more rolls can be removed
+            }
+
+            for (int[] pos : toRemove) {
+                int r = pos[0];
+                int c = pos[1];
+                if (grid[r][c] == '@') { // still present
+                    grid[r][c] = '.';
+                    removedTotal++;
                 }
             }
         }
 
-        System.out.println(accessibleCount);
+        System.out.println(removedTotal);
     }
 }
